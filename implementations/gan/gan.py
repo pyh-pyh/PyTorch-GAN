73d659c3511a5ca1e9aad3b21f50f843ee20a2bf
pyh-pyh <2335257900@qq.com>
2021-12-08 21:50:24 +0800

    formattin&try

--- implementations/gan/gan.py
+++ implementations/gan/gan.py
@@ -17,16 +17,46 @@ import torch
 os.makedirs("images", exist_ok=True)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
-parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
-parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
-parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
-parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
-parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
-parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
-parser.add_argument("--img_size", type=int, default=28, help="size of each image dimension")
-parser.add_argument("--channels", type=int, default=1, help="number of image channels")
-parser.add_argument("--sample_interval", type=int, default=400, help="interval betwen image samples")
+parser.add_argument("--n_epochs",
+                    type=int,
+                    default=200,
+                    help="number of epochs of training")
+parser.add_argument("--batch_size",
+                    type=int,
+                    default=64,
+                    help="size of the batches")
+parser.add_argument("--lr",
+                    type=float,
+                    default=0.0002,
+                    help="adam: learning rate")
+parser.add_argument("--b1",
+                    type=float,
+                    default=0.5,
+                    help="adam: decay of first order momentum of gradient")
+parser.add_argument("--b2",
+                    type=float,
+                    default=0.999,
+                    help="adam: decay of first order momentum of gradient")
+parser.add_argument("--n_cpu",
+                    type=int,
+                    default=8,
+                    help="number of cpu threads to use during batch generation")
+parser.add_argument("--latent_dim",
+                    type=int,
+                    default=100,
+                    help="dimensionality of the latent space")
+parser.add_argument("--img_size",
+                    type=int,
+                    default=28,
+                    help="size of each image dimension")
+parser.add_argument("--channels",
+                    type=int,
+                    default=1,
+                    help="number of image channels")
+parser.add_argument("--sample_interval",
+                    type=int,
+                    default=400,
+                    help="interval betwen image samples")
 opt = parser.parse_args()
 print(opt)
 
@@ -46,14 +76,11 @@ class Generator(nn.Module):
             layers.append(nn.LeakyReLU(0.2, inplace=True))
             return layers
 
-        self.model = nn.Sequential(
-            *block(opt.latent_dim, 128, normalize=False),
-            *block(128, 256),
-            *block(256, 512),
-            *block(512, 1024),
-            nn.Linear(1024, int(np.prod(img_shape))),
-            nn.Tanh()
-        )
+        self.model = nn.Sequential(*block(opt.latent_dim, 128, normalize=False),
+                                   *block(128, 256), *block(256, 512),
+                                   *block(512, 1024),
+                                   nn.Linear(1024, int(np.prod(img_shape))),
+                                   nn.Tanh())
 
     def forward(self, z):
         img = self.model(z)
@@ -95,22 +122,28 @@ if cuda:
 
 # Configure data loader
 os.makedirs("../../data/mnist", exist_ok=True)
-dataloader = torch.utils.data.DataLoader(
+dataloader = DataLoader(
     datasets.MNIST(
         "../../data/mnist",
         train=True,
         download=True,
-        transform=transforms.Compose(
-            [transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
-        ),
+        transform=transforms.Compose([
+            transforms.Resize(opt.img_size),
+            transforms.ToTensor(),
+            transforms.Normalize([0.5], [0.5])
+        ]),
     ),
     batch_size=opt.batch_size,
     shuffle=True,
 )
 
 # Optimizers
-optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
-optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+optimizer_G = torch.optim.Adam(generator.parameters(),
+                               lr=opt.lr,
+                               betas=(opt.b1, opt.b2))
+optimizer_D = torch.optim.Adam(discriminator.parameters(),
+                               lr=opt.lr,
+                               betas=(opt.b1, opt.b2))
 
 Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 
@@ -122,7 +155,8 @@ for epoch in range(opt.n_epochs):
     for i, (imgs, _) in enumerate(dataloader):
 
         # Adversarial ground truths
-        valid = Variable(Tensor(imgs.size(0), 1).fill_(1.0), requires_grad=False)
+        valid = Variable(Tensor(imgs.size(0), 1).fill_(1.0),
+                         requires_grad=False)
         fake = Variable(Tensor(imgs.size(0), 1).fill_(0.0), requires_grad=False)
 
         # Configure input
@@ -135,7 +169,8 @@ for epoch in range(opt.n_epochs):
         optimizer_G.zero_grad()
 
         # Sample noise as generator input
-        z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], opt.latent_dim))))
+        z = Variable(
+            Tensor(np.random.normal(0, 1, (imgs.shape[0], opt.latent_dim))))
 
         # Generate a batch of images
         gen_imgs = generator(z)
@@ -160,11 +195,13 @@ for epoch in range(opt.n_epochs):
         d_loss.backward()
         optimizer_D.step()
 
-        print(
-            "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
-            % (epoch, opt.n_epochs, i, len(dataloader), d_loss.item(), g_loss.item())
-        )
+        print("[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]" %
+              (epoch, opt.n_epochs, i, len(dataloader), d_loss.item(),
+               g_loss.item()))
 
         batches_done = epoch * len(dataloader) + i
         if batches_done % opt.sample_interval == 0:
-            save_image(gen_imgs.data[:25], "images/%d.png" % batches_done, nrow=5, normalize=True)
+            save_image(gen_imgs.data[:25],
+                       "images/%d.png" % batches_done,
+                       nrow=5,
+                       normalize=True)
